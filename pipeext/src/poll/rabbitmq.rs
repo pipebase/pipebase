6fb822fab55328ea90828800e9077cd773f4a85d
Li Yu <li.yu.sh0211@gmail.com>
2021-06-11 09:21:51 -0700

    improve rabbitmq test

--- pipeext/src/poll/rabbitmq.rs
+++ pipeext/src/poll/rabbitmq.rs
@@ -5,8 +5,9 @@ use lapin::{
     options::BasicAckOptions, types::FieldTable, Connection, ConnectionProperties, Consumer,
 };
 use log::{error, info};
-use pipebase::FromConfig;
 use pipebase::Poll;
+use pipebase::{FromConfig, FromFile};
+use serde::Deserialize;
 use std::error::Error;
 use std::result::Result;
 use tokio_amqp::*;
@@ -19,11 +20,14 @@ pub struct RabbitMQConsumer {
     channel: Channel,
 }
 
+#[derive(Deserialize)]
 pub struct RabbitMQConsumerConfig {
     pub queue: String,
     pub consumer_tag: String,
     pub uri: String,
 }
+
+impl FromFile for RabbitMQConsumerConfig {}
 
 #[async_trait]
 impl FromConfig<RabbitMQConsumerConfig> for RabbitMQConsumer {
@@ -47,16 +51,7 @@ impl FromConfig<RabbitMQConsumerConfig> for RabbitMQConsumer {
 #[async_trait]
 impl Poll<Vec<u8>> for RabbitMQConsumer {
     async fn poll(&mut self) -> Result<Option<Vec<u8>>, Box<dyn Error + Send + Sync>> {
-        let consumer = match self
-            .channel
-            .basic_consume(
-                &self.queue,
-                &self.consumer_tag,
-                self.options.to_owned(),
-                self.args.to_owned(),
-            )
-            .await
-        {
+        let consumer = match self.create_consumer().await {
             Ok(consumer) => consumer,
             Err(err) => return Err(err.into()),
         };
@@ -74,13 +69,23 @@ impl Poll<Vec<u8>> for RabbitMQConsumer {
     }
 }
 
+impl RabbitMQConsumer {
+    pub async fn create_consumer(&self) -> std::result::Result<Consumer, lapin::Error> {
+        self.channel
+            .basic_consume(
+                &self.queue,
+                &self.consumer_tag,
+                self.options.to_owned(),
+                self.args.to_owned(),
+            )
+            .await
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
-    use pipebase::FromConfig;
-    use pipebase::{Pipe, Source};
-    use std::println as info;
-    use std::sync::Arc;
+    use pipebase::{source, FromConfig, FromFile, Pipe, Source};
     use tokio::sync::mpsc::channel;
     use tokio::sync::mpsc::Receiver;
 
@@ -103,23 +108,16 @@ mod tests {
     #[ignore]
     async fn test_consumer() {
         let (tx, mut rx) = channel::<Vec<u8>>(1024);
-        let config = RabbitMQConsumerConfig {
-            queue: "foo".to_owned(),
-            consumer_tag: "foo".to_owned(),
-            uri: "amqp://127.0.0.1:5672/%2f".to_owned(),
-        };
-        let rbmq = RabbitMQConsumer::from_config(&config).await.unwrap();
-        let mut s = Source {
-            name: "rbmq_consumer",
-            txs: vec![Arc::new(tx)],
-            poller: Box::new(rbmq),
-            context: Default::default(),
-        };
-
+        let mut s = source!(
+            "rbmq_consumer",
+            "resources/catalogs/rabbitmq_consumer.yml",
+            RabbitMQConsumerConfig,
+            RabbitMQConsumer,
+            [tx]
+        );
         let jh0 = tokio::spawn(async move {
             s.run().await;
         });
-
         let jh1 = on_receive(&mut rx);
         tokio::join!(jh0, jh1);
     }
