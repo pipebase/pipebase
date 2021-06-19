b273b551aeb2bdfec7e65571b1140fea3a9e0831
Li Yu <li.yu.sh0211@gmail.com>
2021-06-19 08:44:22 -0700

    simplify exporter

--- pipebase/src/sink/mod.rs
+++ pipebase/src/sink/mod.rs
@@ -7,13 +7,13 @@ use std::marker::PhantomData;
 use async_trait::async_trait;
 use log::error;
 use std::sync::Arc;
-use tokio::sync::mpsc::{channel, Receiver};
-use tokio::sync::{Mutex, RwLock};
+use tokio::sync::mpsc::Receiver;
+use tokio::sync::RwLock;
 
 use crate::context::State;
 use crate::Pipe;
+use crate::Result;
 use crate::{context::Context, ConfigInto, FromConfig};
-use crate::{error::join_error, Result};
 
 #[async_trait]
 pub trait Export<T: Send + Sync + 'static, C>: Send + Sync + FromConfig<C> {
@@ -25,7 +25,7 @@ pub trait Export<T: Send + Sync + 'static, C>: Send + Sync + FromConfig<C> {
 
 pub struct Exporter<'a, T: Send + Sync + 'static, E: Export<T, C>, C: ConfigInto<E>> {
     pub name: &'a str,
-    pub rx: Arc<Mutex<Receiver<T>>>,
+    pub rx: Receiver<T>,
     pub config: C,
     pub exporter: PhantomData<E>,
     pub context: Arc<RwLock<Context>>,
@@ -37,54 +37,31 @@ impl<'a, T: Send + Sync + 'static, E: Export<T, C> + 'static, C: ConfigInto<E> +
 {
     async fn run(&mut self) -> Result<()> {
         let mut exporter = self.config.config_into().await.unwrap();
-        let (tx_e, mut rx_e) = channel::<T>(1024);
-        let rx = self.rx.to_owned();
-        let event_loop = tokio::spawn(async move {
-            let mut rx = rx.lock().await;
-            loop {
-                let t = match rx.recv().await {
-                    Some(t) => t,
-                    None => break,
-                };
-                match tx_e.send(t).await {
-                    Ok(_) => continue,
-                    Err(err) => {
-                        error!("send exporter failed {}", err);
-                        break;
-                    }
+
+        loop {
+            Self::inc_total_run(self.context.to_owned()).await;
+            Self::set_state(self.context.to_owned(), State::Receive).await;
+            let t = match self.rx.recv().await {
+                Some(t) => t,
+                None => {
+                    Self::inc_success_run(self.context.to_owned()).await;
+                    break;
+                }
+            };
+            Self::set_state(self.context.to_owned(), State::Export).await;
+            match exporter.export(&t).await {
+                Ok(_) => {
+                    Self::inc_success_run(self.context.to_owned()).await;
+                }
+                Err(err) => {
+                    error!("export error {}", err);
+                    break;
                 }
             }
-        });
-        let context = self.context.to_owned();
-        let export_loop = tokio::spawn(async move {
-            loop {
-                Self::inc_total_run(context.to_owned()).await;
-                Self::set_state(context.to_owned(), State::Receive).await;
-                let t = match rx_e.recv().await {
-                    Some(t) => t,
-                    None => {
-                        Self::inc_success_run(context.to_owned()).await;
-                        break;
-                    }
-                };
-                Self::set_state(context.to_owned(), State::Export).await;
-                match exporter.export(&t).await {
-                    Ok(_) => {
-                        Self::inc_success_run(context.to_owned()).await;
-                    }
-                    Err(err) => {
-                        error!("export error {}", err);
-                        break;
-                    }
-                }
-            }
-            Self::set_state(context.to_owned(), State::Done).await;
-        });
-        let join_all = tokio::spawn(async move { tokio::join!(event_loop, export_loop) });
-        match join_all.await {
-            Ok(_) => Ok(()),
-            Err(err) => Err(join_error(err)),
         }
+        Self::set_state(self.context.to_owned(), State::Done).await;
+
+        Ok(())
     }
 
     fn get_context(&self) -> Arc<RwLock<Context>> {
@@ -101,7 +78,7 @@ macro_rules! exporter {
             <$config>::from_file($path).expect(&format!("invalid config file location {}", $path));
         let pipe = Exporter {
             name: $name,
-            rx: std::sync::Arc::new(tokio::sync::Mutex::new($rx)),
+            rx: $rx,
             config: config,
             exporter: std::marker::PhantomData,
             context: Default::default(),
