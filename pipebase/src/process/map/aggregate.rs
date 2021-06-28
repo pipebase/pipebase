271bbd47b60a215ff6435d63907b1c1473ad5346
Li Yu <li.yu.sh0211@gmail.com>
2021-06-28 11:36:18 -0700

    Impl group sum aggreator

--- pipebase/src/process/map/aggregate.rs
+++ pipebase/src/process/map/aggregate.rs
@@ -1,7 +1,10 @@
+use std::hash::Hash;
+
 use super::Map;
 use crate::{ConfigInto, FromConfig, FromPath};
 use async_trait::async_trait;
 use serde::Deserialize;
+use std::collections::HashMap;
 pub trait Aggregate<I, T, U>
 where
     T: IntoIterator<Item = I>,
@@ -35,13 +38,13 @@ impl FromConfig<SumAggregatorConfig> for SumAggregator {
 
 impl<I, T> Aggregate<I, T, I> for SumAggregator
 where
-    I: std::ops::Add<Output = I> + Default,
+    I: std::ops::AddAssign<I> + Default,
     T: IntoIterator<Item = I>,
 {
     fn aggregate(&self, t: T) -> I {
         let mut sum: I = Default::default();
         for item in t.into_iter() {
-            sum = sum.add(item);
+            sum += item;
         }
         sum
     }
@@ -50,7 +53,7 @@ where
 #[async_trait]
 impl<I, T> Map<T, I, SumAggregatorConfig> for SumAggregator
 where
-    I: std::ops::Add<Output = I> + Default,
+    I: std::ops::AddAssign<I> + Default,
     T: IntoIterator<Item = I> + Send + 'static,
 {
     async fn map(&mut self, data: T) -> anyhow::Result<I> {
@@ -59,7 +62,7 @@ where
 }
 
 #[cfg(test)]
-mod tests {
+mod aggregator_tests {
     use crate::*;
     use tokio::sync::mpsc::Sender;
 
@@ -81,5 +84,92 @@ mod tests {
         assert_eq!(16, odd);
         let even = rx1.recv().await.unwrap();
         assert_eq!(20, even);
+    }
+}
+
+pub trait GroupAggregate<I, T, U>
+where
+    I: Hash + Eq + PartialEq,
+    T: IntoIterator<Item = I>,
+{
+    fn group_aggregate(&self, t: T) -> U;
+}
+
+#[derive(Deserialize)]
+pub struct GroupSumAggregatorConfig {}
+
+impl FromPath for GroupSumAggregatorConfig {
+    fn from_path<P>(_path: P) -> anyhow::Result<Self>
+    where
+        P: AsRef<std::path::Path>,
+    {
+        Ok(GroupSumAggregatorConfig {})
+    }
+}
+
+#[async_trait]
+impl ConfigInto<GroupSumAggregator> for GroupSumAggregatorConfig {}
+
+#[async_trait]
+impl FromConfig<GroupSumAggregatorConfig> for GroupSumAggregator {
+    async fn from_config(_config: &GroupSumAggregatorConfig) -> anyhow::Result<Self> {
+        Ok(GroupSumAggregator {})
+    }
+}
+
+pub struct GroupSumAggregator {}
+
+impl<I, T> GroupAggregate<I, T, HashMap<I, I>> for GroupSumAggregator
+where
+    I: Hash + Eq + PartialEq + std::ops::AddAssign<I> + Clone,
+    T: IntoIterator<Item = I>,
+{
+    fn group_aggregate(&self, t: T) -> HashMap<I, I> {
+        let mut group_sum: HashMap<I, I> = HashMap::new();
+        for ref item in t {
+            match group_sum.get_mut(item) {
+                Some(sum) => *sum += item.to_owned(),
+                None => {
+                    group_sum.insert(item.to_owned(), item.to_owned());
+                }
+            };
+        }
+        group_sum
+    }
+}
+
+#[async_trait]
+impl<I, T> Map<T, HashMap<I, I>, GroupSumAggregatorConfig> for GroupSumAggregator
+where
+    I: Hash + Eq + PartialEq + std::ops::AddAssign<I> + Clone,
+    T: IntoIterator<Item = I> + Send + 'static,
+{
+    async fn map(&mut self, data: T) -> anyhow::Result<HashMap<I, I>> {
+        Ok(self.group_aggregate(data))
+    }
+}
+
+#[cfg(test)]
+mod test_group_aggregator {
+    use crate::*;
+    use tokio::sync::mpsc::Sender;
+
+    async fn populate_record(tx: Sender<Vec<u32>>, records: Vec<Vec<u32>>) {
+        for record in records {
+            let _ = tx.send(record).await;
+        }
+    }
+
+    #[tokio::test]
+    async fn test_group_sum_aggregator() {
+        let (tx0, rx0) = channel!(Vec<u32>, 1024);
+        let (tx1, mut rx1) = channel!(HashMap<u32, u32>, 1024);
+        let mut pipe = mapper!("group_summation", GroupSumAggregatorConfig, rx0, [tx1]);
+        let f0 = populate_record(tx0, vec![vec![2, 3, 2, 3, 2]]);
+        f0.await;
+        spawn_join!(pipe);
+        let gs = rx1.recv().await.unwrap();
+        assert_eq!(&6, gs.get(&2).unwrap());
+        assert_eq!(&6, gs.get(&3).unwrap());
     }
 }
