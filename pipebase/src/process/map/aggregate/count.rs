b558f638d504d95d43f5cf4a6d3cbbb2ab7a026c
Li Yu <li.yu.sh0211@gmail.com>
2021-07-12 19:43:11 -0700

    Impl unordered group count32

--- pipebase/src/process/map/aggregate/count.rs
+++ pipebase/src/process/map/aggregate/count.rs
@@ -1,7 +1,9 @@
-use crate::{Aggregate, AggregateAs, ConfigInto, FromConfig, FromPath, Map};
+use crate::{
+    Aggregate, AggregateAs, ConfigInto, FromConfig, FromPath, GroupAggregate, GroupAs, Map, Pair,
+};
 use async_trait::async_trait;
 use serde::Deserialize;
-use std::{cmp::Ordering, fmt::Debug};
+use std::{cmp::Ordering, collections::HashMap, fmt::Debug, hash::Hash, iter::FromIterator};
 
 #[derive(Clone, Debug, Eq)]
 pub struct Count32(pub u32);
@@ -129,5 +131,108 @@ mod count32_tests {
         join_pipes!([pipe]);
         let c = rx1.recv().await.expect("count32 not found");
         assert_eq!(4, c.get())
+    }
+}
+
+#[derive(Deserialize)]
+pub struct UnorderedGroupCount32AggregatorConfig {}
+
+#[async_trait::async_trait]
+impl FromPath for UnorderedGroupCount32AggregatorConfig {
+    async fn from_path<P>(_path: P) -> anyhow::Result<Self>
+    where
+        P: AsRef<std::path::Path> + Send,
+    {
+        Ok(UnorderedGroupCount32AggregatorConfig {})
+    }
+}
+
+impl ConfigInto<UnorderedGroupCount32Aggregator> for UnorderedGroupCount32AggregatorConfig {}
+
+pub struct UnorderedGroupCount32Aggregator {}
+
+#[async_trait::async_trait]
+impl FromConfig<UnorderedGroupCount32AggregatorConfig> for UnorderedGroupCount32Aggregator {
+    async fn from_config(_config: &UnorderedGroupCount32AggregatorConfig) -> anyhow::Result<Self> {
+        Ok(UnorderedGroupCount32Aggregator {})
+    }
+}
+
+impl<I, T, K, U> GroupAggregate<I, T, K, Count32, U, HashMap<K, Count32>>
+    for UnorderedGroupCount32Aggregator
+where
+    I: GroupAs<K> + AggregateAs<Count32>,
+    T: IntoIterator<Item = I>,
+    K: Hash + Eq + PartialEq,
+    U: FromIterator<Pair<K, Count32>>,
+{
+    fn merge(&self, v: &mut Count32, i: &I) {
+        *v += i.aggregate_value()
+    }
+
+    fn group_table(&self) -> anyhow::Result<HashMap<K, Count32>> {
+        Ok(HashMap::new())
+    }
+}
+
+#[async_trait]
+impl<I, T, K> Map<T, Vec<Pair<K, Count32>>, UnorderedGroupCount32AggregatorConfig>
+    for UnorderedGroupCount32Aggregator
+where
+    I: GroupAs<K> + AggregateAs<Count32>,
+    T: IntoIterator<Item = I> + Send + 'static,
+    K: Hash + Eq + PartialEq,
+{
+    async fn map(&mut self, data: T) -> anyhow::Result<Vec<Pair<K, Count32>>> {
+        Ok(self.group_aggregate(data)?)
+    }
+}
+
+#[cfg(test)]
+mod group_count32_tests {
+
+    use crate::*;
+
+    #[derive(Debug, Clone, GroupAs, AggregateAs)]
+    #[agg(count32)]
+    struct Record {
+        #[group]
+        key: String,
+    }
+
+    #[tokio::test]
+    async fn test_group_count32() {
+        let (tx0, rx0) = channel!(Vec<Record>, 1024);
+        let (tx1, mut rx1) = channel!(Vec<Pair<String, Count32>>, 1024);
+        let mut pipe = mapper!("group_count32");
+        let pipe = run_pipe!(pipe, UnorderedGroupCount32AggregatorConfig, [tx1], rx0);
+        let f0 = populate_records(
+            tx0,
+            vec![vec![
+                Record {
+                    key: "foo".to_owned(),
+                },
+                Record {
+                    key: "foo".to_owned(),
+                },
+                Record {
+                    key: "bar".to_owned(),
+                },
+            ]],
+        );
+        f0.await;
+        join_pipes!([pipe]);
+        let group_counts = rx1.recv().await.expect("group count32 not found");
+        for count in group_counts {
+            match &count.left()[..] {
+                "foo" => {
+                    assert_eq!(2, count.right().get())
+                }
+                "bar" => {
+                    assert_eq!(1, count.right().get())
+                }
+                _ => unreachable!("unexpected group {}", count.left()),
+            }
+        }
     }
 }
