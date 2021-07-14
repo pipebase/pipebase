767046f8265a2c99224888d2f6fd48b04ed15f80
Li Yu <li.yu.sh0211@gmail.com>
2021-07-13 18:38:37 -0700

    factor out psql client

--- pipeext/src/utils/mod.rs
+++ pipeext/src/utils/mod.rs
@@ -1,7 +1,9 @@
 mod byteops;
+mod psqlclient;
 mod redisclient;
 mod rocksdbclient;
 
 pub use byteops::*;
+pub use psqlclient::*;
 pub use redisclient::*;
 pub use rocksdbclient::*;
---
+++ pipeext/src/utils/psqlclient.rs
@@ -0,0 +1,29 @@
+use pipebase::Render;
+use tokio_postgres::{Client, NoTls};
+pub struct PsqlClient {
+    client: Client,
+}
+
+impl PsqlClient {
+    // params schema: https://github.com/sfackler/rust-postgres/blob/master/postgres/src/config.rs
+    // type supoort: https://docs.rs/postgres/0.19.1/postgres/types/trait.ToSql.html
+    pub async fn new(params: &str) -> anyhow::Result<Self> {
+        let (client, connection) = tokio_postgres::connect(params, NoTls).await?;
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
+        Ok(PsqlClient { client: client })
+    }
+
+    pub async fn execute<R>(&mut self, record: R) -> anyhow::Result<()>
+    where
+        R: Render,
+    {
+        let statement = record.render();
+        let rows_updated = self.client.execute(&statement[..], &[]).await?;
+        log::info!("{} rows updated", rows_updated);
+        Ok(())
+    }
+}
