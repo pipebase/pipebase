176ad4ad36377a4ded0de7b1daac09216caa2c79
Li Yu <li.yu.sh0211@gmail.com>
2021-07-26 08:39:47 -0700

    Impl KubeEventReader

--- pipeware/pipekube/src/listen.rs
+++ pipeware/pipekube/src/listen.rs
@@ -1,11 +1,13 @@
 use crate::model::*;
 use async_trait::async_trait;
+use chrono::{DateTime, Utc};
 use futures::{StreamExt, TryStreamExt};
-use k8s_openapi::api::core::v1::Pod;
+use k8s_openapi::api::core::v1::{Event, Pod};
 use kube::{
-    api::{Api, LogParams},
+    api::{Api, ListParams, LogParams},
     Client,
 };
+use kube_runtime::{utils::try_flatten_applied, watcher};
 use pipebase::{
     common::{ConfigInto, FromConfig, FromPath},
     listen::Listen,
@@ -82,5 +84,73 @@ impl KubeLogReader {
                 None => (),
             }
         }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct KubeEventReaderConfig {
+    namespace: Option<String>,
+}
+
+#[async_trait]
+impl FromPath for KubeEventReaderConfig {}
+
+impl ConfigInto<KubeEventReader> for KubeEventReaderConfig {}
+
+pub struct KubeEventReader {
+    events: Api<Event>,
+    tx: Option<Sender<KubeEvent>>,
+}
+
+#[async_trait]
+impl FromConfig<KubeEventReaderConfig> for KubeEventReader {
+    async fn from_config(config: KubeEventReaderConfig) -> anyhow::Result<Self> {
+        let client = Client::try_default().await?;
+        let events: Api<Event> = match config.namespace {
+            Some(ref ns) => Api::namespaced(client, ns),
+            None => Api::all(client),
+        };
+        Ok(KubeEventReader { events, tx: None })
+    }
+}
+
+#[async_trait]
+impl Listen<KubeEvent, KubeEventReaderConfig> for KubeEventReader {
+    async fn run(&mut self) -> anyhow::Result<()> {
+        self.do_run().await
+    }
+
+    fn set_sender(&mut self, sender: Sender<KubeEvent>) {
+        self.tx = Some(sender)
+    }
+}
+
+impl KubeEventReader {
+    async fn do_run(&self) -> anyhow::Result<()> {
+        let params = ListParams::default();
+        let mut watcher = try_flatten_applied(watcher(self.events.to_owned(), params)).boxed();
+        let tx = self
+            .tx
+            .as_ref()
+            .expect("sender not inited for kube event reader");
+        while let Some(event) = watcher.try_next().await? {
+            let namespace = event.involved_object.namespace.unwrap_or_default();
+            let kind = event.involved_object.kind.unwrap_or_default();
+            let name = event.involved_object.name.unwrap_or_default();
+            let message = event.message.unwrap_or_default();
+            let action = event.action.unwrap_or_default();
+            let count = event.count.unwrap_or_default();
+            let component = event.reporting_component.unwrap_or_default();
+            let instance = event.reporting_instance.unwrap_or_default();
+            let event_time: DateTime<Utc> = match event.event_time {
+                Some(event_time) => event_time.0,
+                None => Utc::now(),
+            };
+            tx.send(KubeEvent::new(
+                namespace, kind, name, message, action, count, component, instance, event_time,
+            ))
+            .await?;
+        }
+        Ok(())
     }
 }
--- pipeware/pipekube/src/model.rs
+++ pipeware/pipekube/src/model.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use pipebase::common::{Convert, Pair};
 use serde::{Deserialize, Serialize};
 
@@ -30,15 +31,46 @@ impl Convert<KubeLog> for Pair<String, String> {
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct KubeEvent {
+    pub namespace: String,
     pub kind: String,
     pub name: String,
     pub message: String,
+    pub action: String,
+    pub count: i32,
+    pub component: String,
+    pub instance: String,
+    pub event_time: DateTime<Utc>,
 }
 
-impl Convert<KubeEvent> for Pair<String, String> {
+impl Convert<KubeEvent> for Pair<String, KubeEvent> {
     fn convert(rhs: KubeEvent) -> Self {
-        let l = format!("{}/{}", rhs.kind, rhs.name);
-        let r = rhs.message;
-        Pair::<String, String>::new(l, r)
+        let l = format!("{}/{}/{}", rhs.namespace, rhs.kind, rhs.name);
+        Pair::<String, KubeEvent>::new(l, rhs)
+    }
+}
+
+impl KubeEvent {
+    pub fn new(
+        namespace: String,
+        kind: String,
+        name: String,
+        message: String,
+        action: String,
+        count: i32,
+        component: String,
+        instance: String,
+        event_time: DateTime<Utc>,
+    ) -> Self {
+        KubeEvent {
+            namespace,
+            kind,
+            name,
+            message,
+            action,
+            count,
+            component,
+            instance,
+            event_time,
+        }
     }
 }
