9849a345e227aeb2be19ee963908beb079847041
Li Yu <li.yu.sh0211@gmail.com>
2021-07-26 18:59:31 -0700

    Store RedisAveragef32 as string value

--- pipeware/piperedis/src/map/aggregate/avg.rs
+++ pipeware/piperedis/src/map/aggregate/avg.rs
@@ -6,29 +6,48 @@ use std::ops::AddAssign;
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct RedisAveragef32(pub Averagef32);
 
+impl RedisAveragef32 {
+    fn new(sum: f32, count: f32) -> Self {
+        RedisAveragef32(Averagef32::new(sum, count))
+    }
+
+    fn to_literal(&self) -> String {
+        let sum = self.0.sum();
+        let count = self.0.count();
+        format!("{},{}", sum, count)
+    }
+
+    fn from_literal(lit: String) -> Self {
+        let sum_count: Vec<&str> = lit.split(',').collect();
+        let sum: f32 = sum_count
+            .get(0)
+            .expect("no sum found for RedisAveragef32")
+            .parse()
+            .expect("failed to parse sum as f32");
+        let count: f32 = sum_count
+            .get(1)
+            .expect("no count found for RedisAveragef32")
+            .parse()
+            .expect("failed to parse count as f32");
+        Self::new(sum, count)
+    }
+}
+
 impl ToRedisArgs for RedisAveragef32 {
     fn write_redis_args<W>(&self, out: &mut W)
     where
         W: ?Sized + redis::RedisWrite,
     {
-        let sum = self.0.sum();
-        let count = self.0.count();
-        let avg: Vec<f32> = vec![sum, count];
-        avg.write_redis_args(out);
-    }
-
-    fn is_single_arg(&self) -> bool {
-        false
+        let avg_lit = self.to_literal();
+        avg_lit.write_redis_args(out);
     }
 }
 
 impl FromRedisValue for RedisAveragef32 {
     fn from_redis_value(v: &redis::Value) -> redis::RedisResult<Self> {
-        let sum_and_count = Vec::<f32>::from_redis_value(v)?;
-        let sum = sum_and_count.get(0).expect("sum not found");
-        let count = sum_and_count.get(1).expect("count not found");
-        let avgf32 = Averagef32::new(sum.to_owned(), count.to_owned());
-        Ok(RedisAveragef32(avgf32))
+        let lit = String::from_redis_value(v)?;
+        let avg = RedisAveragef32::from_literal(lit);
+        Ok(avg)
     }
 }
 
