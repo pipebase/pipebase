e1f991d9bf9573baa45b60da7f28799d534e07fe
Li Yu <li.yu.sh0211@gmail.com>
2021-11-27 11:49:16 -0800

    [e2e/ingest_redis] itest

--- e2e/clients/src/constants.rs
+++ e2e/clients/src/constants.rs
@@ -1,3 +1,6 @@
 pub const CONTEXT_SERVER_HEALTH: &str = "/v1/health";
 pub const CONTEXT_SERVER_SHUTDOWN: &str = "/v1/shutdown";
 pub const CONTEXT_SERVER_PIPE: &str = "/v1/pipe";
+pub const INGESTION_SERVER_HEALTH: &str = "/v1/health";
+pub const INGESTION_SERVER_INGEST: &str = "/v1/ingest";
+pub const INGESTION_SERVER_SHUTDOWN: &str = "/v1/shutdown";
--- e2e/clients/src/lib.rs
+++ e2e/clients/src/lib.rs
@@ -1,7 +1,9 @@
 mod constants;
 mod httpcli;
+mod rediscli;
 mod utils;
 
 pub use constants::*;
 pub use httpcli::*;
+pub use rediscli::*;
 pub use utils::*;
