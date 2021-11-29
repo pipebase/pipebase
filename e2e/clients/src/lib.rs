370ac9a25c69d9dfca82a23c722d02d40cb04523
Li Yu <li.yu.sh0211@gmail.com>
2021-11-29 11:49:45 -0800

    [e2e/ingest_mysql] itest

--- e2e/clients/src/lib.rs
+++ e2e/clients/src/lib.rs
@@ -1,11 +1,13 @@
 mod constants;
 mod cqlcli;
 mod httpcli;
+mod mysqlcli;
 mod rediscli;
 mod utils;
 
 pub use constants::*;
 pub use cqlcli::*;
 pub use httpcli::*;
+pub use mysqlcli::*;
 pub use rediscli::*;
 pub use utils::*;
